58209340c083102e6a7539b07d04e10ca02828be
felix nkandu <felixnkandu@gmail.com>
2020-11-12 20:10:10 +0200

    add user menu

---
+++ bmk/business_operations.py
@@ -0,0 +1,23 @@
+# def add_record():
+def a():
+    title = input('enter title : ')
+    return title
+
+
+# a()
+
+def b():
+    url = input('enter url : ')
+    return url
+
+
+# b()
+
+def c():
+    desc = input('enter description : ')
+    return desc
+
+# c()
+
+
+# add_record()
---
+++ bmk/clients.py
@@ -0,0 +1,10 @@
+from business_operations import a, b, c
+
+a()
+b()
+c()
+
+# def create_record():
+
+
+# create_record()
